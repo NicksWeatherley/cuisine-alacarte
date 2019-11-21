7817c3f8bda526bf205f3733e65fbc91c48b009e
Natalia <natalia@Roberts-MacBook-Pro.local>
2019-11-21 17:59:27 -0500

    added cook model. added cook to settings

--- cuisine_alacarte/settings.py
+++ cuisine_alacarte/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cook.apps.CookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
