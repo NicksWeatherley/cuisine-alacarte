8c0ad98f2e24e6512c2f9303207f4e8293a0e528
Natalia <natalia@Roberts-MacBook-Pro.local>
2019-11-22 15:37:18 -0500

    created restaurant model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=200)
+    manager = models.ForeignKey('manager.Manager')
+    menu = models.ForeignKey('menu')
+    # I don't think we need a cook row here. cooks point to restaurants
+    # We'll have to add types of customers (vip, blocked etc)
+    customers = models.ManyToManyField('customer.Customer') # we might later put this in Customer model instead
