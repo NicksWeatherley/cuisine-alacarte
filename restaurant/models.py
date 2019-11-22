53c805a8aa41513eb82782ca5788c1e111de84e4
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-11-22 15:58:12 -0500

    Added on_delete params to foreign keys

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=40)
     address = models.CharField(max_length=200)
-    manager = models.ForeignKey('manager.Manager')
-    menu = models.ForeignKey('menu')
+    manager = models.ForeignKey('manager.Manager', on_delete=models.CASCADE)
+    menu = models.ForeignKey('menu', on_delete=models.CASCADE)
     # I don't think we need a cook row here. cooks point to restaurants
     # We'll have to add types of customers (vip, blocked etc)
     customers = models.ManyToManyField('customer.Customer') # we might later put this in Customer model instead
