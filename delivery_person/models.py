78f2e631f1c74b591bc6c05e68bfde4b0649b422
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-11-22 15:01:09 -0500

    Added models to delivery person

--- delivery_person/models.py
+++ delivery_person/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class DeliveryPerson(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    ssn = models.CharField(max_length=9)
+#   where they work, will reference restaurants table
+    #restaurant = models.ForeignKey('restaurant.Restaurant')
+#   rating (0 - 5) will reference Rating table/app
+    #rating = models.ForeignKey('ratings.Rating') # referencing model defined in different app
+#   if cook gets warned more than 3 times they get a warning
+    warnings = models.IntegerField()
+    salary = models.FloatField()
+
 # Create your models here.
