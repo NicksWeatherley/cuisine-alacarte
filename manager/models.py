1d95b22f87d56c6baf7b73fd861c8b79cf05b8b7
RMeruelo <Raulameruelo@hotmail.com>
2019-11-22 16:09:39 -0500

    changed SSN to ssn

--- manager/models.py
+++ manager/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Manager(models.model):
     first_name = models.CharField(max_lenght = 20)
     last_name =models.CharField(max_lenght = 30)
-    SSN = models.CharField(max_lenght = 9)
+    ssn = models.CharField(max_lenght = 9)
     resturant = models.Foreig0nKey('restaurant.Restaurant', on_delete = models.CASCADE)
     salary = models.FloatField()
