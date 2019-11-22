5b0b4cc3ed8d7e7f7e00044488153ce12d5c3e5e
Natalia <natalia@Roberts-MacBook-Pro.local>
2019-11-22 14:46:46 -0500

    created restaurant app

--- cuisine_alacarte/settings.py
+++ cuisine_alacarte/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'restaurant.apps.RestaurantConfig',
     'customer.apps.CustomerConfig',
     'delivery_person.apps.DeliveryPersonConfig',
     'cook.apps.CookConfig',
