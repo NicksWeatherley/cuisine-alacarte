92a954064ce3f1eb7937300299768934c900f039
RMeruelo <Raulameruelo@hotmail.com>
2019-11-22 16:26:33 -0500

    added basics to ratings model

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Rating(models.model):
+    score = models.FloatField()
+    resturant = models.Foreig0nKey('restaurant.Restaurant', on_delete = models.CASCADE)
