b908c09bd4a7e3c4697ba69b195aad7fe6e2c5b3
Natalia <natalia@Roberts-MacBook-Pro.local>
2019-11-22 17:19:29 -0500

    fixed migrate errors

--- item/models.py
+++ item/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=40)
-    price = models.DecimalField()
-    rating = models.ForeignKey('rating.Ratings', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.ForeignKey('ratings.Rating', on_delete=models.CASCADE)
     restaurant = models.ForeignKey('restaurant.Restaurant', on_delete=models.CASCADE)
