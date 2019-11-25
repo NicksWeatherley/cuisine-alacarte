6b8a364cc1970691d3e05c69d53593cbc9e5a362
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-11-25 15:15:21 -0500

    Restaurant refactor. Item refactor

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -5,6 +5,35 @@ from ratings.models import Rating
 from cook.models import Cook
 from delivery_person.models import DeliveryPerson
 from salesperson.models import Salesperson
+from item.models import Item
+from customer.models import Customer
+
+
+class CookInLine(admin.TabularInline):
+    model = Cook
+    classes = ['collapse', ]
+    max_num = 3
+    extra = 1
+
+class CustomerInLine(admin.TabularInline):
+    model = Customer
+    classes = ['collapse', ]
+    max_num = 3
+    extra = 0
+
+
+class DeliveryPersonInLine(admin.TabularInline):
+    model = DeliveryPerson
+    classes = ['collapse', ]
+    max_num = 3
+    extra = 1
+
+
+class ItemsInLine(admin.TabularInline):
+    model = Item
+    classes = ['collapse', ]
+    max_num = 3
+    extra = 0
 
 
 class RatingsInLine(admin.TabularInline):
@@ -14,39 +43,28 @@ class RatingsInLine(admin.TabularInline):
     extra = 0
 
 
-class CookInLine(admin.TabularInline):
-    model = Cook
-    classes = ['collapse', ]
-    max_num = 3
-    extra = 1
-
-
-class DeliveryPersonInLine(admin.TabularInline):
-    model = DeliveryPerson
-    classes = ['collapse', ]
-    max_num = 3
-    extra = 1
-
 class SalesPersonInLine(admin.TabularInline):
     model = Salesperson
     classes = ['collapse', ]
     max_num = 3
     extra = 1
 
+
 class RestaurantAdmin(admin.ModelAdmin):
     inlines = (
         CookInLine,
         DeliveryPersonInLine,
         SalesPersonInLine,
+        ItemsInLine,
+        CustomerInLine,
         RatingsInLine,
-
     )
 
-    fieldsets = (('Info', {
+    fieldsets = (('Contact Info', {
         'fields': ('name', 'address', 'manager',),
         'classes': ('extrapretty'),
     }),
-    ('Employees',{'fields':()}),)
+        ('Data', {'fields': ()}),)
 
 
 admin.site.register(Restaurant, RestaurantAdmin)
