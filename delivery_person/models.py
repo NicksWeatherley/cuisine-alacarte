81e378dc82ac38ae7efe2b8df1f7428e2147a5d8
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-11-24 22:23:51 -0500

    Refactored admin page of cook and restaurant.
    Restaurant now has collapsable views to edit/view cooks and ratings. Should be updated
    so that ratings can not be edited. Cook model was deregistred as can view on restaurant
    page.

--- delivery_person/models.py
+++ delivery_person/models.py
@@ -5,7 +5,7 @@ class DeliveryPerson(models.Model):
     last_name = models.CharField(max_length=30)
     ssn = models.CharField(max_length=9)
 #   where they work, will reference restaurants table
-    #restaurant = models.ForeignKey('restaurant.Restaurant')
+   # restaurant = models.ForeignKey('restaurant.Restaurant')
 #   rating (0 - 5) will reference Rating table/app
     #rating = models.ForeignKey('ratings.Rating') # referencing model defined in different app
 #   if cook gets warned more than 3 times they get a warning
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 
 from .models import Restaurant
+from ratings.models import Rating
+from cook.models import Cook
 
-admin.site.register(Restaurant)
+class RatingsInLine(admin.TabularInline):
+    model = Rating
+    classes = ['collapse',]
+    max_num = 10
+    extra = 0
+
+class CookInLine(admin.TabularInline):
+    model = Cook
+    classes = ['collapse',]
+    max_num = 3
+    extra = 1
+
+class RestaurantAdmin(admin.ModelAdmin):
+    inlines = (
+        CookInLine,
+        RatingsInLine,
+    )
+
+    fieldsets = ((None, {
+        'fields': ('name', 'address', 'manager',),
+        'classes': ('extrapretty'),
+    }),)
+
+admin.site.register(Restaurant, RestaurantAdmin)
