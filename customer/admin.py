ef93ec2fa3f7f05b8e9af4bd3f53aa1c3671cc56
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-12-07 11:17:42 -0500

    Added note field to rating model

--- customer/admin.py
+++ customer/admin.py
@@ -6,5 +6,3 @@ from .models import Customer, Order
 
 admin.site.register(Customer)
 admin.site.register(Order)
-
-
--- ratings/models.py
+++ ratings/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
 class Rating(models.Model):
     score = models.FloatField()
-    resturant = models.ForeignKey('restaurant.Restaurant', on_delete = models.CASCADE)
+    note = models.CharField(
+        max_length=120,
+        blank=True,
+        default="",
+        )
+
+    # Keeping this here as not to force clear migrations or DB, but not needed
+    resturant = models.ForeignKey(
+        "restaurant.Restaurant",
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+    )
