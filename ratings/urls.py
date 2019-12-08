a231f706d68e85b43f004318ecebe276dca9a55b
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-12-07 19:08:07 -0500

    Added views for all ratings

--- ratings/urls.py
+++ ratings/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 import ratings.views as view
 
 urlpatterns = [
-    path('ratings/Item/<int:item_id>', view.ItemRatingListView.as_view(), name='item_rating_list'),
+    path('ratings/Customer/<int:item_id>',
+         view.CustomerRatingListView.as_view(), name='customer_rating_list'),
+    path('ratings/Delivery/<int:item_id>',
+         view.DeliveryRatingListView.as_view(), name='Delivery_rating_list'),
+    path('ratings/Dish/<int:item_id>',
+         view.DishRatingListView.as_view(), name='dish_rating_list'),
+    path('ratings/Item/<int:item_id>',
+         view.ItemRatingListView.as_view(), name='item_rating_list'),
 ]
--- ratings/views.py
+++ ratings/views.py
@@ -6,26 +6,42 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
 from django.contrib.auth import get_user_model
 
-from .models import ItemRating
+from .models import ItemRating, CustomerRating, DeliveryRating, DishRating
 from customer.models import Customer
 from delivery_person.models import Delivery
 from cook.models import Cook
 from item.models import Item, Dish
 
-@method_decorator([login_required], name='dispatch')
-class UserRatingListView(ListView):
-    model = ItemRating
-
-    def get_queryset(self, **kwargs):
-        querry_type = type(None)
-        if(querry_type == type(None)):
-            pass
-
 class ItemRatingListView(ListView):
     model = ItemRating
+    template_name = 'ratings/ratings_list.html'
 
     def get_queryset(self, **kwargs):
-        print(self.kwargs['item_id'])
-        print('\n\n\n\n\n')
         ritem = Item.objects.get(id=self.kwargs['item_id'])
         return ritem.itemrating_set.all()
+
+
+class CustomerRatingListView(ListView):
+    model = CustomerRating
+    template_name = 'ratings/rating_list.html'
+
+    def get_queryset(self, **kwargs):
+        cust = Customer.objects.get(id=self.kwargs['item_id'])
+        return cust.customerrating_set.all()
+
+
+class DeliveryRatingListView(ListView):
+    model = DeliveryRating
+    template_name = 'ratings/rating_list.html'
+
+    def get_queryset(self, **kwargs):
+        cust = Delivery.objects.get(id=self.kwargs['item_id'])
+        return cust.deliveryrating_set.all()
+
+class DishRatingListView(ListView):
+    model = DishRating
+    template_name = 'ratings/rating_list.html'
+
+    def get_queryset(self, **kwargs):
+        cust = Dish.objects.get(id=self.kwargs['item_id'])
+        return cust.dishrating_set.all()
