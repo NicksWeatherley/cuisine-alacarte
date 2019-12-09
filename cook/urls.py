ff479cd43c64cc0d7cbf86df2854266938d59c45
Natalia <natalia@Roberts-MBP.lan1>
2019-12-08 19:45:01 -0500

    added cook add/delete dishes

--- cook/urls.py
+++ cook/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DishListView
+from .views import DishListView, get_new_dish, get_dishes_to_remove
 
 urlpatterns = [
     path('cook/dishlist', DishListView.as_view(), name='dish_list'),
+    path('cook/adddish/<int:cook_id>', get_new_dish, name='get_new_dish'),
+    path('cook/getdishestoremove/<int:cook_id>', get_dishes_to_remove, name='get_dishes_to_remove' )
 ]
-
 
--- cook/views.py
+++ cook/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
+from django.http import HttpResponseRedirect
+
 from item.models import Dish
 from .models import Cook
+from .forms import NewDishForm, RemoveDishForm
+from item.models import Item
 
 # def view_cook_menu(request):
 #     return render(request, "cook/dish_list.html")
@@ -18,4 +22,51 @@ class DishListView(ListView):
     def get_queryset(self, **kwargs):
         user = Cook.objects.filter(user=self.request.user.id)[0]
         return user.dish_set.all()
+
+
+def get_new_dish(request, cook_id):
+    if request.method == 'POST':
+        form = NewDishForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            price = form.cleaned_data['price']
+            items = form.cleaned_data['items']
+            _add_new_dish(name, price, items, cook_id)
+            return HttpResponseRedirect('/cook/dishlist')
+    else:
+        form = NewDishForm()
+
+    return render(request, 'cook/add_dish.html', {'form': form})
+
+
+def _add_new_dish(name, price, items, cook_id):
+    cook = Cook.objects.all()[cook_id - 1]
+    restaurant = cook.restaurant
+    new_dish = Dish.objects.create(name=name, price=price, cook=cook, restaurant=restaurant)
+
+    for form_item in items:
+        item = Item.objects.all()[int(form_item.id) - 1]
+        new_dish.items.add(item)
+
+    new_dish.save()
+
+
+def get_dishes_to_remove(request, cook_id):
+    form = RemoveDishForm(request.user, request.POST)
+    if form.is_valid():
+        dishes = form.cleaned_data['dishes']
+        _remove_dishes(dishes)
+        return HttpResponseRedirect('/cook/dishlist')
+    else:
+        form = RemoveDishForm(request.user)
+
+    return render(request, 'cook/remove_dish.html', {'form': form})
+
+
+def _remove_dishes(dishes):
+    # for form_dish in dishes:
+    #     dish = Dish.objects.all()[int(form_dish.id) - 1]
+    #     dish.delete()
+    dishes.delete()
+
 
