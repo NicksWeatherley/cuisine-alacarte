2526ced0a62cee7dc51d660cabe9ec2d426d7446
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-12-10 15:13:30 -0500

    Added way for cook to rate item

--- item/views.py
+++ item/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Item
+from cook.models import Cook
+
+@method_decorator([login_required], name='dispatch')
+class ItemListView(ListView):
+    model = Item
+    template_name = 'ratings/item_list.html'
+
+    def get_queryset(self, **kwargs):
+        user = Cook.objects.filter(user=self.request.user.id)[0]
+        return Item.objects.filter(restaurant = user.restaurant.id)
