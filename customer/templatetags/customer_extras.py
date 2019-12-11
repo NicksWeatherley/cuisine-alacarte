0b1f240f68810346d51ad69e90ea3701fa4eb963
Terri <territhampan@gmail.com>
2019-12-11 18:11:43 -0500

    adding tts branch -not functioning

--- customer/templatetags/customer_extras.py
+++ customer/templatetags/customer_extras.py
@@ -1,15 +1,20 @@
 from django import template
 from gtts import gTTS
 import os
+import html2text
+from urllib.request import urlopen
 
 
 register = template.Library()
 
 @register.simple_tag
-def texttospeech(text):
+def texttospeech(url):
      #if request.method == 'POST':
-        speech = gTTS(text= text, lang='en')
-        speech.save("test.mp3")
-        os.system("start test.mp3")
-        #text = "tts activated"
-        #return "test.mp3"
+    f = urlopen(url)
+    html = f.read()
+    text = html2text.html2text(html)
+    speech = gTTS(text= text, lang='en')
+    speech.save("test.mp3")
+    os.system("start test.mp3")
+    #text = "tts activated"
+    #return "test.mp3"
