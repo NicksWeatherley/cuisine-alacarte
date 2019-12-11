459889aac58fa510beea3879e2a1e3071cf34cdd
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-12-10 22:12:53 -0500

    Added change pay feature

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CustomerListView, EmployeeList
+from .views import CustomerListView, EmployeeList, EditPayCook, EditPayDeliveryPerson
 
 urlpatterns = [
     path('manager/customerlist', CustomerListView.as_view(), name='customer_list'),
     path('manager/employeelist', EmployeeList.as_view(), name='employee_list'),
-
+    path('manager/cookpay/<int:cook_id>', EditPayCook, name='cook_salary_change'),
+    path('manager/deliverypersonpay/<int:delivery_person_id>', EditPayDeliveryPerson, name='delivery_person_salary_change'),    
 ]
