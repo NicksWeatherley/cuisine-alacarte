b403efee89f855a68e0f0364324817db89dc331f
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-12-10 20:24:03 -0500

    Added views/pages for manager to view employees

--- manager/urls.py
+++ manager/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import CustomerListView
+from .views import CustomerListView, EmployeeList
 
 urlpatterns = [
     path('manager/customerlist', CustomerListView.as_view(), name='customer_list'),
+    path('manager/employeelist', EmployeeList.as_view(), name='employee_list'),
+
 ]
